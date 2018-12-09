897233ac91227b7b7a8000e0cdbb9923d58d907a
Ahmed Nawar <ahmednawar2007@gmail.com>
2018-12-09 12:42:20 +0200

    used promises

--- entities/hotel.js
+++ entities/hotel.js
@@ -1,24 +1,28 @@
+<<<<<<< HEAD
+=======
+function getAllHotels(req) {
+  var sql = "SELECT * FROM hotels, facilities where approved = ? AND hotels.id = facilities.hotel_id;";
+  return new Promise((resolve, reject) => {
+    req.con.query(sql, [1], function(err, hotels) {
+        if(err) console.log(err);
+        else{
+         resolve(hotels);    
+        }  
+    });
+});
+}
+
+>>>>>>> used promises
 function insertHotel(req) {
-  var query =
-    "INSERT INTO  hotels (name, location, owner_id, stars, description) VALUES (?,?,?,?,?)";
+  var query ="INSERT INTO  hotels (name, location, owner_id, stars, description) VALUES (?,?,?,?,?)";
 
   return new Promise((resolve, reject) => {
     console.log(req.user);
-    req.con.query(
-      query,
-      [
-        req.body.hotelname,
-        req.body.location,
-        req.user.id,
-        req.body.stars,
-        req.body.description
-      ],
-      (err, rows) => {
+    req.con.query(query, [req.body.hotelname, req.body.location, req.user.id, req.body.stars, req.body.description], (err, rows) => {
         console.log(req.user);
         if (err) throw err;
         resolve();
-      }
-    );
+      });
   });
 }
 
@@ -44,7 +48,13 @@ function getAllApprovedHotelsWithFacilities(req) {
 }
 
 module.exports = {
+<<<<<<< HEAD
   insertHotel: insertHotel,
   getAllApprovedHotels: getAllApprovedHotels,
   getAllApprovedHotelsWithFacilities: getAllApprovedHotelsWithFacilities
 };
+=======
+  getAllHotels: getAllHotels,
+  insertHotel: insertHotel
+};
+>>>>>>> used promises
--- notes.md
+++ notes.md
@@ -1,1 +1,34 @@
-Ay haga
+This is a backup for the horizontal hotels view for later use
+
+<!-- 
+
+<button type="button" class="btn btn-outline-dark stacked_view">Stacked View</button>
+<button type="button" class="btn btn-outline-dark horizontal_view">Horizontal View</button>
+
+
+
+<div class="card mb-3 all_hotels" name="">
+    <div class="row"  style="overflow:hidden">
+        <img class=" hotel-image col-3 img-fluid" src="/images/photo2.jpg" alt="Hotel image cap">
+        <div class="col-4"><strong>Facilities</strong>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item  py-0">Pool: <%=hotel.pool===1?'Yes':'No' %></li>
+            <li class="list-group-item  py-0">Restaraunt: <%=hotel.restaurant===1?'Yes':'No' %></li>
+            <li class="list-group-item  py-0">Bar: <%=hotel.bar===1?'Yes':'No' %></li>
+          </ul>
+        </div>
+        <div class="col-4"><ul class="list-group list-group-flush">
+            <li class="list-group-item  py-0">Wifi: <%=hotel.wifi===1?'Yes':'No' %></li>
+            <li class="list-group-item  py-0">Gym: <%=hotel.gym===1?'Yes':'No' %></li>
+            <li class="list-group-item  py-0">Kids Area: <%=hotel.kids_area===1?'Yes':'No' %></li>
+            <li class="list-group-item  py-0">Spa: <%=hotel.spa===1?'Yes':'No' %></li>
+          </ul>
+        </div>
+    </div>
+    <div class="card-body">
+        <h5 class="card-title"><%=hotel.name%></h5>
+        <p class="card-text"><%=hotel.description%></p>
+    </div>
+</div> -->
+
+
