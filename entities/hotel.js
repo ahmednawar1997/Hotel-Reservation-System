d983e9972b59e681fcfdacda64efbe94f4ef8de6
OmarKatary <OmarKatary@users.noreply.github.com>
2018-12-09 15:18:51 +0200

    Adding facilities to hotel regestration

--- entities/hotel.js
+++ entities/hotel.js
@@ -1,28 +1,57 @@
-<<<<<<< HEAD
-=======
-function getAllHotels(req) {
-  var sql = "SELECT * FROM hotels, facilities where approved = ? AND hotels.id = facilities.hotel_id;";
-  return new Promise((resolve, reject) => {
-    req.con.query(sql, [1], function(err, hotels) {
-        if(err) console.log(err);
-        else{
-         resolve(hotels);    
-        }  
-    });
-});
-}
-
->>>>>>> used promises
 function insertHotel(req) {
-  var query ="INSERT INTO  hotels (name, location, owner_id, stars, description) VALUES (?,?,?,?,?)";
+  var query1 =
+    "INSERT INTO  hotels (name, location, owner_id, stars, description) VALUES (?,?,?,?,?)";
+  var query2 =
+    "INSERT INTO  facilities (hotel_id, pool, restaurant, bar, gym, kids_area, spa) VALUES (?,?,?,?,?,?,?)";
+  var query3 = "SELECT id FROM hotels WHERE owner_id=? AND name=?";
+  facilities = {
+    pool: req.body.pool ? 1 : 0,
+    restaurant: req.body.restaurant ? 1 : 0,
+    bar: req.body.bar ? 1 : 0,
+    gym: req.body.gym ? 1 : 0,
+    kidsArea: req.body.kidsArea ? 1 : 0,
+    spa: req.body.spa ? 1 : 0
+  };
 
   return new Promise((resolve, reject) => {
     console.log(req.user);
-    req.con.query(query, [req.body.hotelname, req.body.location, req.user.id, req.body.stars, req.body.description], (err, rows) => {
-        console.log(req.user);
+    req.con.query(
+      query1,
+      [
+        req.body.hotelname,
+        req.body.location,
+        req.user.id,
+        req.body.stars,
+        req.body.description
+      ],
+      (err, rows) => {
         if (err) throw err;
-        resolve();
-      });
+        req.con.query(
+          query3,
+          [req.user.id, req.body.hotelname],
+          (err, rows) => {
+            if (err) throw err;
+            var hotelId = rows[0].id;
+            req.con.query(
+              query2,
+              [
+                hotelId,
+                facilities.pool,
+                facilities.restaurant,
+                facilities.bar,
+                facilities.gym,
+                facilities.kidsArea,
+                facilities.spa
+              ],
+              err => {
+                if (err) throw err;
+                resolve();
+              }
+            );
+          }
+        );
+      }
+    );
   });
 }
 
@@ -48,13 +77,7 @@ function getAllApprovedHotelsWithFacilities(req) {
 }
 
 module.exports = {
-<<<<<<< HEAD
   insertHotel: insertHotel,
   getAllApprovedHotels: getAllApprovedHotels,
   getAllApprovedHotelsWithFacilities: getAllApprovedHotelsWithFacilities
 };
-=======
-  getAllHotels: getAllHotels,
-  insertHotel: insertHotel
-};
->>>>>>> used promises
