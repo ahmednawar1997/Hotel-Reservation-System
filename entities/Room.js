67f7faad83677296c7c35447d34cf2108a7898e0
Ahmed Nawar <ahmednawar2007@gmail.com>
2018-12-22 12:06:11 +0200

    add max available rooms while booking

--- entities/Room.js
+++ entities/Room.js
@@ -25,9 +25,30 @@ function insertRoom(req) {
 
 
 function getNumberOfRooms(req, hotel_id, checkin, checkout){
-  var query = "";
+  var query = "SELECT number_of_reserved_rooms.room_type,total_number_of_rooms.room_view,total_number_of_rooms.price,sum1-sum2 AS free_rooms FROM "+
+  "(SELECT room_type.room_type,room_type.room_view,room_type.price,SUM(room_type.number_of_rooms) AS sum1 "+
+  "FROM room_type "+
+  "WHERE room_type.hotel_id=? "  +
+  "GROUP BY room_type.room_type,room_type.room_view,room_type.price)AS total_number_of_rooms "+
+  "LEFT OUTER JOIN " +
+  "(SELECT hotel_reservation_ids.room_type,SUM(hotel_reservation_ids.number_of_rooms) AS sum2 "+
+  "FROM " +
+  "(SELECT reserved_rooms.* FROM reserved_rooms " +
+  "INNER JOIN "+
+  "(SELECT reservations.reservation_id FROM reservations "+
+  "WHERE reservations.hotel_id=? " +
+   "AND " +
+  "((? >= reservations.check_in_date AND ? < reservations.check_out_date) "+
+  "OR (? <= reservations.check_in_date AND ? > reservations.check_in_date AND ? < reservations.check_out_date) "+
+  "OR (? >= reservations.check_in_date AND ? < reservations.check_out_date)) "+
+  ")AS input_conditions "+
+  "ON "+
+  "reserved_rooms.reservation_id=input_conditions.reservation_id)AS hotel_reservation_ids "+
+   "GROUP BY hotel_reservation_ids.room_type) AS number_of_reserved_rooms "+
+   "ON number_of_reserved_rooms.room_type=total_number_of_rooms.room_type;";
+
   return new Promise((resolve, reject) => {
-    req.con.query(query, [checkin, checkin, hotel_id, checkout, checkout, hotel_id, checkin, checkout, hotel_id ],
+    req.con.query(query, [hotel_id, hotel_id, checkin, checkin, checkin, checkout , checkin , checkin , checkout],
       (err, availableRooms) => {
         if (err) throw err;
         console.log("AVAILABLE ROOMS");
@@ -46,5 +67,5 @@ function getNumberOfRooms(req, hotel_id, checkin, checkout){
 module.exports = {
     getRoomsByHotelId,
     insertRoom,
-    getAvailableRoomsIfReserved
+    getNumberOfRooms
   };
