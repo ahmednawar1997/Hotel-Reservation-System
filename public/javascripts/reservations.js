fa7407c12b2f01a78d1bec092d5c3d9e634b4d8b
OmarKatary <OmarKatary@users.noreply.github.com>
2018-12-25 15:19:40 +0200

    Fixed reservations bug

--- public/javascripts/reservations.js
+++ public/javascripts/reservations.js
@@ -1,26 +1,34 @@
 
-function approveReservation(resID) {
+function approveReservation(res) {
     $(function () {
-
+        var reservation = JSON.parse(res);
         $.ajax({
             url: '/user/owner/reservations',
             method: 'POST',
-            data: { reservation_id: resID, hotel_approval: 1 },
+            data: { reservation_id: reservation.reservation_id, hotel_approval: 1 },
             success: () => {
-                $('#approveButton' + resID).remove();
-                $('#cancelButton' + resID).remove();
-                $('#color' + resID).toggleClass('pending_circle checkmark_circle');
-                $('.check1' + resID).addClass('checkmark_stem')
-                $('.check2' + resID).addClass('checkmark_kick')
-                $('#checkInDiv' + resID).html('<div class="card-footer" align="center">\
+
+                $('#approveButton' + reservation.reservation_id).remove();
+                $('#cancelButton' + reservation.reservation_id).remove();
+                $('#color' + reservation.reservation_id).toggleClass('pending_circle checkmark_circle');
+                $('.check1' + reservation.reservation_id).addClass('checkmark_stem')
+                $('.check2' + reservation.reservation_id).addClass('checkmark_kick')
+
+                var now = new Date();
+                var date = new Date(reservation.check_in_date);
+
+                if (reservation.checked_in === null && date < now) {
+                    $('#checkInDiv' + reservation.reservation_id).html(
+                        '<div class="card-footer" align="center">\
                     <span>Guest Checked In ? </span>\
                     <br>\
                         <button type="button" class="btn btn-success btn-sm" style="margin-right:5px" id="yesButton<%=detailedReservations[i].reservation_id%>"\
-                            onclick="guestCheckedIn('+ resID + ')"><i class="fas fa-check-circle"></i> Yes</button>\
-                        <button type="button" class="btn btn-danger btn-sm" style="" id="noButton<%=detailedReservations[i].reservation_id%>" onclick="guestNotCheckedIn('+ resID + ')">\
+                            onclick="guestCheckedIn('+ reservation.reservation_id + ')"><i class="fas fa-check-circle"></i> Yes</button>\
+                        <button type="button" class="btn btn-danger btn-sm" style="" id="noButton<%=detailedReservations[i].reservation_id%>" onclick="guestNotCheckedIn('+ reservation.reservation_id + ')">\
                             <i class="fas fa-times-circle"></i>\
                             No</button>\
                         </div>')
+                }
 
             }
         });
