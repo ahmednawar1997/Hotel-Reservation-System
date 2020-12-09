164cb81d7948df4dc202cd6bb428ea8ae7710aca
Ahmed Nawar <ahmednawar2007@gmail.com>
2020-12-09 22:28:11 +0200

    Change gitignore to remove node_modules

--- helpers/authorization.js
+++ helpers/authorization.js
@@ -1,37 +1,37 @@
 function isAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect("/login");
-  }
-  
-  function isCustomer(req, res, next) {
-    if (req.user.type !== 'customer') {
-      req.flash('message', 'You don\'t have authorization to complete this action');
-      res.redirect("/home");
-      return;
-      //req.path
-    }
-    return next();
-  }
-  
-  function isBroker(req, res, next) {
-    if (req.user.type !== 'broker') {
-      req.flash('message', 'You don\'t have authorization to complete this action');
-      res.redirect("/home");
-      return;
-      //req.path
-    }
-    return next();
-  }
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
+}
 
-  
+function isCustomer(req, res, next) {
+  if (req.user.type !== 'customer') {
+    req.flash('message', 'You don\'t have authorization to complete this action');
+    res.redirect("/home");
+    return;
+    //req.path
+  }
+  return next();
+}
+
+function isBroker(req, res, next) {
+  if (req.user.type !== 'broker') {
+    req.flash('message', 'You don\'t have authorization to complete this action');
+    res.redirect("/home");
+    return;
+    //req.path
+  }
+  return next();
+}
+
+
 function isHotelOwner(req, res, next) {
-    if (req.user.type !== 'hotel_owner' && req.user.type !== 'broker') {
-        req.flash('message', 'You don\'t have authorization to complete this action');
-        res.redirect("/login");
-        return;
-        //req.path
-    }
-    return next();
+  if (req.user.type !== 'hotel_owner' && req.user.type !== 'broker') {
+    req.flash('message', 'You don\'t have authorization to complete this action');
+    res.redirect("/login");
+    return;
+    //req.path
+  }
+  return next();
 }
 function isBlacklisted(req, res, next) {
 
@@ -39,16 +39,16 @@ function isBlacklisted(req, res, next) {
     req.flash('message', 'You Are Blacklisted');
     res.redirect("/login");
     return;
-}
-return next();
+  }
+  return next();
 
 }
 
 
 module.exports = {
-    isHotelOwner,
-    isBroker,
-    isAuthenticated,
-    isCustomer,
-    isBlacklisted
+  isHotelOwner,
+  isBroker,
+  isAuthenticated,
+  isCustomer,
+  isBlacklisted
 };
